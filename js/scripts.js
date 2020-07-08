6be7ea7cab2083947a0652b0376002cbffdb934a
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-07-08 15:06:12 -0600

    Assigned value to variable inputText

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,7 @@ function ButtonClick_InputText() {
     
     // @link https://www.w3schools.com/js/js_dates.asp
     // @link https://stackoverflow.com/questions/27869606/remove-time-from-gmt-time-format
+    // @link https://github.com/TECHCareers-by-Manpower/js-practice/commit/b5822168f20a8a98c20095728bd7af71175cbbeb
     var newDate = new Date().toLocaleString();
     var dateTime = newDate;
     let inputText = document.getElementById("newtask").value;
@@ -42,17 +43,17 @@ function ButtonClick_InputText() {
     {
         error.innerText=""; 
       
-        let newItem =document.createElement("li");
-        newItem.innerText = inputText.value.trim();
+        //let newItem =document.createElement("li");
+        //newItem.innerText = inputText.value.trim();
 
-        listArr.push(inputText.value.trim().toLowerCase());
+        //listArr.push(inputText.value.trim().toLowerCase());
 
-        if(list.className === "emptyList")
+       /* if(list.className === "emptyList")
         {
             list.classList.remove("emptyList");
             list.children[0].remove();
         }
-            list.appendChild(newItem);
+            list.appendChild(newItem);*/
             inputText.value= "";
             inputText.focus();
     }
