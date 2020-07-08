74ed989ea65ba8a6d25c4225df896220b477ac73
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-07-08 12:02:39 -0600

    Assigned value to variable inputText

--- js/scripts.js
+++ js/scripts.js
@@ -7,55 +7,50 @@ const PBox1 = document.querySelector("box1");
 const comptTask = document.querySelector("compTask");
 //const inputText=document.querySelector("newTask");
 
-let inputText="";
+let inputText;
 let addedText;
 let compPara;
-let listArr=[];
+let listArr = [];
 
 
 
 function ButtonClick_InputText() {
 
-    
+
     // @link https://www.w3schools.com/js/js_dates.asp
     // @link https://stackoverflow.com/questions/27869606/remove-time-from-gmt-time-format
     var newDate = new Date().toLocaleString();
     var dateTime = newDate;
-    
+    let inputText = document.getElementById("newtask").value;
+    let addedText = document.getElementById('box1').innerHTML = "<p>" + inputText + " " + "<b>" + "Start:" + "</b>" + " " + dateTime + "</p>";
 
     let inputError = false;
     if (inputText.value.trim() === "") {
         error.innerText = "Sorry, please add an item.";
         inputError = true;
-        }
+    }
 
-    if(listArr.includes(inputText.value.trim().toLowercase()))
-    {
+    if (listArr.includes(inputText.value.trim().toLowercase())) {
         error.innerText = "Sorry, the item already exists.";
         inputError = true;
     }
 
-    if(inputText.value.trim() !== "" && !listArr.includes(inputText.value.trim().toLowercase()))
+    if (inputText.value.trim() !== "" && !listArr.includes(inputText.value.trim().toLowercase()))
 
-    if(!inputError)
-    {
-        error.innerText="";
-        let inputText = document.getElementById("newtask").value;
-        let addedText = document.getElementById('box1').innerHTML = "<p>" + inputText + " " + "<b>" + "Start:" + "</b>" + " " + dateTime + "</p>";
-       
-      
+        if (!inputError) {
+            error.innerText = "";
 
-        listArr.push(inputText.value.trim().toLowercase());
 
-        if(list.className=== "emptyList")
-        {
-            list.classList.remove("emptyList");
-            list.children[0].remove();
-        }
-            inputText.value= "";
+            listArr.push(inputText.value.trim().toLowercase());
+
+            if (list.className === "emptyList") {
+                list.classList.remove("emptyList");
+                list.children[0].remove();
+            }
+            inputText.value = "";
             inputText.focus();
-    }
-    
+        }
+
 
 }
 
