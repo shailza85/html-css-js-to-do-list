40e2f4f4fa6a6eecfb3a4405b9878307a8af3b7d
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-07-08 11:16:20 -0600

    Added if statement and input error message.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,65 @@
-function ButtonClick_InputText()
-{
-    var inputText = document.getElementById("newtask").value;
-      
-    document.getElementById('box1').innerHTML="<p>" +inputText+ "</p>";
+
+const list = document.querySelector("ul");
+const addTask = document.querySelector("chkbox");
+const para = document.querySelector("p");
+const frm = document.querySelector("form");
+const PBox1 = document.querySelector("box1");
+const comptTask = document.querySelector("compTask");
+
+var inputText;
+var addedText;
+var compPara;
+let listArr=[];
+
+
+
+function ButtonClick_InputText() {
+
+    
+    // @link https://www.w3schools.com/js/js_dates.asp
+    // @link https://stackoverflow.com/questions/27869606/remove-time-from-gmt-time-format
+    var newDate = new Date().toLocaleString();
+    var dateTime = newDate;
+    
+
+    let inputError = false;
+    if (inputText.value.trim() === "") {
+        error.innerText = "Sorry, please add an item.";
+        inputError = true;
+        }
+
+    if(listArr.includes(inputText.value.trim().toLowercase()))
+    {
+        error.innerText = "Sorry, the item already exists.";
+        inputError = true;
+    }
+
+    if(inputText.value.trim() !== "" && !listArr.includes(inputText.value.trim().toLowercase()))
+    
+    if(!inputError)
+    {
+        error.innerText="";
+        inputText = document.getElementById("newtask").value;
+        addedText = document.getElementById('box1').innerHTML = "<p>" + inputText + " " + "<b>" + "Start:" + "</b>" + " " + dateTime + "</p>";
+       
+        cmTask = document.getElementById('compTask').innerHTML = addedText.value.trim();
+
+        listArr.push(inputText.value.trim().toLowercase());
+
+        if(list.className=== "emptyList")
+        {
+            list.classList.remove("emptyList");
+            list.children[0].remove();
+        }
+            inputText.value= "";
+            inputText.focus();
+    }
+    
 
 }
 
-/*function AddTask_List()
-{
-    var text = document.querySelector("taskInsert");
-    var addtime =new Date();
-    var startstr = addtime.toLocaleDateString();
-    text.innerHTML+= "Start" + ":" + startstr + inputText + "<br/>"
-}*/
+//function AddTask_List() {}
+
+
+
+
