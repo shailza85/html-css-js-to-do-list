537c911e8239aa23b386f3f2c6e02a5a153fd519
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-07-10 10:58:34 -0600

    Added css styles and Delete button function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,95 +1,121 @@
 
 const list = document.querySelector("ul");
-const addTask = document.querySelector("chkbox");
-const para = document.querySelector("p");
-const frm = document.querySelector("form");
 const inputButton = document.querySelector("#addInput");
 const comptTask = document.querySelector("compTask");
 const error = document.querySelector("#listError");
 const input = document.querySelector("#newtask");
-const clearTask = document.querySelector("#deletebutton");
+const delButton = document.querySelector("#deletebutton");
+const delButton1 = document.querySelector("#deletebutton1");
+const clearTask = document.querySelector("#addTask");
 
 
 let inputText;
-let addedText;
-let compPara;
 let listArr = [];
 
 
-inputButton.addEventListener("click", function() {
+inputButton.addEventListener("click", function () {
     //function ButtonClick_InputText() {
 
 
-        // @link https://www.w3schools.com/js/js_dates.asp
-        // @link https://stackoverflow.com/questions/27869606/remove-time-from-gmt-time-format
-        // @link https://github.com/TECHCareers-by-Manpower/js-practice/commit/b5822168f20a8a98c20095728bd7af71175cbbeb
-        var newDate = new Date().toLocaleString();
-        var dateTime = newDate;
-        
-        
-        inputText = document.querySelector("#newtask").value;
+    // @link https://www.w3schools.com/js/js_dates.asp
+    // @link https://stackoverflow.com/questions/27869606/remove-time-from-gmt-time-format
+    // @link https://github.com/TECHCareers-by-Manpower/js-practice/commit/b5822168f20a8a98c20095728bd7af71175cbbeb
+    
+    
+    var newDate = new Date().toLocaleString();
+    var dateTime = newDate;
 
-        let inputError = false;
-        if (inputText.trim() === "") {
-            error.innerText = "Sorry, no item is enetered. Please type in to add an item.";
-            inputError = true;
+
+    inputText = document.querySelector("#newtask").value;
+
+    let inputError = false;
+    if (inputText.trim() === "") {
+        error.innerText = "Sorry, no item is entered. Please type in to add an item.";
+        inputError = true;
+       
+
+    }
+
+
+    if (listArr.includes(inputText.trim().toLowerCase())) {
+
+        error.innerText = "Sorry, the item already exists. Please ensure you are adding a new item.";
+        inputError = true;
+        input.value = "";
+    }
+
+
+
+    if (inputText.trim() !== "" && !listArr.includes(inputText.trim().toLowerCase()))
+
+
+
+        if (!inputError) {
+            error.innerText = "";
+
+
+            //let chBox=document.createElement("input");  // @link https://stackoverflow.com/questions/866239/creating-the-checkbox-dynamically-using-javascript
+            // chBox.type="checkbox";
+            //chBox.name="checkbox1";
+            //chBox.value="checkbox";
+            //chBox.id="checkbox1";
+
+
+
+            //@link https://stackoverflow.com/questions/41440446/dynamically-create-delete-buttons
+
+            //let buttonDel =document.createElement("button"); 
+            //buttonDel.type="button";
+            //buttonDel.name="delete";
+            // buttonDel.value="delete";
+            // buttonDel.id="delete";
+
+
+
+            let newItem = document.createElement("li");
+            newItem.innerHTML = inputText.trim() + " " + "<b>" + "Start:" + "</b>" + " " + dateTime;
+
+            listArr.push(inputText.trim().toLowerCase());
+
+            // list.appendChild(chBox);
+
+            // list.appendChild(buttonDel);
+
+            //if (list.className === "emptyList") {
+            //list.classList.remove("emptyList");
+            //list.children[2].remove();
+
+            // }
+            list.appendChild(newItem);
+            input.value = "";
+            input.focus();
             
-           
         }
-                
-
-        if (listArr.includes(inputText.trim().toLowerCase())) {
-            
-            error.innerText = "Sorry, the item already exists.";
-            inputError = true;
-            }
-            
-          
-
-        if (inputText.trim() !== "" && !listArr.includes(inputText.trim().toLowerCase()))
-
-   
-
-            if (!inputError) {
-                error.innerText = "";
-
-                let newItem = document.createElement("li");
-                newItem.innerHTML = inputText.trim() + " " + "<b>" + "Start:" + "</b>" + " " + dateTime;
-               
-                listArr.push(inputText.trim().toLowerCase());
-
-                list.appendChild(newItem);
-
-                if (list.className === "emptyList") {
-                    list.classList.remove("emptyList");
-                    list.children[2].remove();
-
-                }
-                    
-                    input.value ="";
-                    input.focus();  
-                    event.preventDefault();
-            }
     //}
-          
+    event.preventDefault();
 }
 );
-function AddTask_List()
- {
 
-
-    /*let inputError1 = false;
-    if (list.children[2]===inputText) {
-        error.innerText = "Sorry, duplicate item is enetered. Please add a new item.";
-        inputError1 = true; }*/
-
+function AddTask_List() {
     compTask.appendChild(list.children[2]);
- }
+}
 
- clearTask.addEventListener("click", () => {
- 
-    inputText = "";    
- 
-    input.dispatchEvent(new Event("input"));
- 
+// @ link https://github.com/TECHCareers-by-Manpower/js-practice/blob/master/Image%20Gallery/js/scripts.js
+
+delButton.addEventListener("click", () => {
+
+    list.children[2].remove();
+
 });
+
+delButton1.addEventListener("click", () => {
+
+    compTask.children[0].remove();
+
+});
+
+
+
+
+
+
